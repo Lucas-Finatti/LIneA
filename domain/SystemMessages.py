8920749cb6ad3ae9ed2fec1db1563b2e4673578c
Lucas Finatti <lucas.finatti@hotmail.com>
2022-07-13 10:54:02 -0300

    domain created

---
+++ domain/SystemMessages.py
@@ -0,0 +1,21 @@
+
+class SystemMessages:
+
+    def detailed(description, system_message):
+        if issubclass(type(description), Exception):
+            description = str(description)
+
+        system_message['description'] = description
+        return system_message
+
+    class General:
+        COULD_NOT_CONNECT_TO_SERVER = {
+            "error": {"reason": "The requested system server was not avaliable, try again later."}
+        },
+        SERVER_NOT_FOUND = {
+            "error": {"reason": "The requested system server was not found."}
+        },
+        INVALID_TOKEN = {
+            "error": {"reason": "Access denied because your token is invalid."}
+        }
+        
