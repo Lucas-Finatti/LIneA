a9dc65d34ba7bd02643e8bd74685bf0abefda18f
Lucas Finatti <lucas.finatti@hotmail.com>
2022-07-13 23:15:44 -0300

    token changed

--- enviroment.py
+++ enviroment.py
@@ -1,3 +1,3 @@
 class keys:
     api_url = 'https://jsonplaceholder.typicode.com/todos'
-    token = 'adc'
+    token = 'V9yoEjgtt7iUaQP3I4tHrWyIpSJjNQK75Wwjfaj2Se1ch60OsqeFBxo9HUkHn0YC8DjU6lnt7idPpxhvve6yzPy62cf7c2057224'
